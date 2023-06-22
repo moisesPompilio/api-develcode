2c4db6dc2be40ed29a0201f566d9def0d992607f
moisesPompilio <moisesjtmed@gmail.com>
2023-06-22 15:52:43 -0300

    feat: inicialização do projeto

---
+++ src/main/java/com/api/Develcode/DevelcodeApplication.java
@@ -0,0 +1,13 @@
+package com.api.Develcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelcodeApplication.class, args);
+	}
+
+}
