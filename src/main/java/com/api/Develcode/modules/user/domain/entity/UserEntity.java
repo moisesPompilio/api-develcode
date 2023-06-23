a55585b77bacdc42a74cf2fc2a47ee31f74c237b
moisesPompilio <moisesjtmed@gmail.com>
2023-06-23 11:10:02 -0300

    feat(UserCRUD): implentação do UserEntity

---
+++ src/main/java/com/api/Develcode/modules/user/domain/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.api.Develcode.modules.user.domain.entity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.api.Develcode.shared.generic.model.GenericEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class UserEntity implements GenericEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    private String foto;
+
+    private LocalDate dataDeNascimento;
+}
