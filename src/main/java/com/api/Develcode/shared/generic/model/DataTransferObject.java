fd3068477b8c52e045a0f4fb17cb471e581e0d18
moisesPompilio <moisesjtmed@gmail.com>
2023-06-23 10:59:47 -0300

    feat(Generic): implementação do DataTransferObject e GenericEntity para deixar os DTOs e Entidades genericos, para que outra classes genericas possam usar

---
+++ src/main/java/com/api/Develcode/shared/generic/model/DataTransferObject.java
@@ -0,0 +1,6 @@
+package com.api.Develcode.shared.generic.model;
+
+public interface DataTransferObject {
+
+}
+
---
+++ src/main/java/com/api/Develcode/shared/generic/model/GenericEntity.java
@@ -0,0 +1,9 @@
+package com.api.Develcode.shared.generic.model;
+
+import java.util.UUID;
+
+public interface GenericEntity {
+
+    UUID getId();
+
+}
