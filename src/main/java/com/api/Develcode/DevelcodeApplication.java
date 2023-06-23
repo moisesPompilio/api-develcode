96eeca3883f999b4af95b6c1bdbd313250859e53
moisesPompilio <moisesjtmed@gmail.com>
2023-06-23 11:02:12 -0300

    chore: adicionamento de configuração do repositorio

--- src/main/java/com/api/Develcode/DevelcodeApplication.java
+++ src/main/java/com/api/Develcode/DevelcodeApplication.java
@@ -2,8 +2,12 @@ package com.api.Develcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.api.Develcode.config.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class DevelcodeApplication {
 
 	public static void main(String[] args) {
