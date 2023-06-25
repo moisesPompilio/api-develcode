0a4c41112bf2c8622b830b3c30039ab2d0ae689a
moisesPompilio <moisesjtmed@gmail.com>
2023-06-25 13:52:37 -0300

    refactor: retirada da rota de pegar usuario por id

--- src/main/java/com/api/Develcode/modules/user/adapter/Controller/UserController.java
+++ src/main/java/com/api/Develcode/modules/user/adapter/Controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
@@ -41,11 +43,6 @@ public class UserController {
     @GetMapping
     public List<UserOutputModel> GetAllUserController() {
         return this.userService.GetAllUserService();
-    }
-
-    @GetMapping("/{id}")
-    public UserOutputModel GetByIdUserController(@PathVariable("id")String id){
-        return  this.userService.GetByIdUserService(id);
     }
 
     @PutMapping("/{id}")
